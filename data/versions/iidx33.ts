5db692d6db226cb6f0dcefc831c0b01033113554
nanao <nanao014029@gmail.com>
2026-06-26 21:06:08 +0900

    add BPL pack vol.3

--- data/versions/iidx33.ts
+++ data/versions/iidx33.ts
@@ -6,6 +6,567 @@ const data: VersionData = {
   shorthand: "Sparkle Shower",
   songs: [
     {
+      genre: "DRUMSTEP",
+      title: "Astra Blaze",
+      artist: "MK",
+      bpm: {
+        min: 168,
+        max: null,
+      },
+      difficulty: {
+        sp: {
+          beginner: {
+            level: null,
+            options: 0,
+          },
+          normal: {
+            level: 6,
+            options: 1,
+          },
+          hyper: {
+            level: 10,
+            options: 1,
+          },
+          another: {
+            level: 12,
+            options: 5,
+          },
+          leggendaria: {
+            level: null,
+            options: 0,
+          },
+        },
+        dp: {
+          normal: {
+            level: 6,
+            options: 1,
+          },
+          hyper: {
+            level: 10,
+            options: 1,
+          },
+          another: {
+            level: 12,
+            options: 5,
+          },
+          leggendaria: {
+            level: null,
+            options: 0,
+          },
+        },
+      },
+    },
+    {
+      genre: "TRIUMPHAL ANTHEM",
+      title: "blue anthem",
+      artist: "OSTER project feat. 花たん",
+      bpm: {
+        min: 160,
+        max: null,
+      },
+      difficulty: {
+        sp: {
+          beginner: {
+            level: null,
+            options: 0,
+          },
+          normal: {
+            level: 6,
+            options: 1,
+          },
+          hyper: {
+            level: 9,
+            options: 1,
+          },
+          another: {
+            level: 11,
+            options: 1,
+          },
+          leggendaria: {
+            level: null,
+            options: 0,
+          },
+        },
+        dp: {
+          normal: {
+            level: 5,
+            options: 1,
+          },
+          hyper: {
+            level: 9,
+            options: 1,
+          },
+          another: {
+            level: 11,
+            options: 1,
+          },
+          leggendaria: {
+            level: null,
+            options: 0,
+          },
+        },
+      },
+    },
+    {
+      genre: "COLOR DANCE POP",
+      title: "COLOR BURST",
+      artist: "Rainy。",
+      bpm: {
+        min: 170,
+        max: null,
+      },
+      difficulty: {
+        sp: {
+          beginner: {
+            level: 1,
+            options: 0,
+          },
+          normal: {
+            level: 4,
+            options: 1,
+          },
+          hyper: {
+            level: 7,
+            options: 1,
+          },
+          another: {
+            level: 10,
+            options: 1,
+          },
+          leggendaria: {
+            level: null,
+            options: 0,
+          },
+        },
+        dp: {
+          normal: {
+            level: 4,
+            options: 1,
+          },
+          hyper: {
+            level: 7,
+            options: 1,
+          },
+          another: {
+            level: 10,
+            options: 1,
+          },
+          leggendaria: {
+            level: null,
+            options: 0,
+          },
+        },
+      },
+    },
+    {
+      genre: "CYBER ROCK",
+      title: "ESPRIT ONE",
+      artist: "SOUND HOLIC feat. STEVIE(44MAGNUM)",
+      bpm: {
+        min: 170,
+        max: null,
+      },
+      difficulty: {
+        sp: {
+          beginner: {
+            level: null,
+            options: 0,
+          },
+          normal: {
+            level: 6,
+            options: 0,
+          },
+          hyper: {
+            level: 8,
+            options: 1,
+          },
+          another: {
+            level: 11,
+            options: 1,
+          },
+          leggendaria: {
+            level: null,
+            options: 0,
+          },
+        },
+        dp: {
+          normal: {
+            level: 6,
+            options: 0,
+          },
+          hyper: {
+            level: 9,
+            options: 0,
+          },
+          another: {
+            level: 11,
+            options: 0,
+          },
+          leggendaria: {
+            level: null,
+            options: 0,
+          },
+        },
+      },
+    },
+    {
+      genre: "Nu-NRG / FUTUREDISCO",
+      title: "Get Into The Groove feat.WaMi",
+      artist: "DÉ DÉ MOUSE",
+      bpm: {
+        min: 140,
+        max: null,
+      },
+      difficulty: {
+        sp: {
+          beginner: {
+            level: null,
+            options: 0,
+          },
+          normal: {
+            level: 5,
+            options: 0,
+          },
+          hyper: {
+            level: 7,
+            options: 0,
+          },
+          another: {
+            level: 10,
+            options: 4,
+          },
+          leggendaria: {
+            level: null,
+            options: 0,
+          },
+        },
+        dp: {
+          normal: {
+            level: 5,
+            options: 0,
+          },
+          hyper: {
+            level: 7,
+            options: 0,
+          },
+          another: {
+            level: 10,
+            options: 4,
+          },
+          leggendaria: {
+            level: null,
+            options: 0,
+          },
+        },
+      },
+    },
+    {
+      genre: "GLHF",
+      title: "GO!",
+      artist: "Juggernaut.",
+      bpm: {
+        min: 128,
+        max: null,
+      },
+      difficulty: {
+        sp: {
+          beginner: {
+            level: null,
+            options: 0,
+          },
+          normal: {
+            level: 6,
+            options: 1,
+          },
+          hyper: {
+            level: 9,
+            options: 5,
+          },
+          another: {
+            level: 11,
+            options: 4,
+          },
+          leggendaria: {
+            level: null,
+            options: 0,
+          },
+        },
+        dp: {
+          normal: {
+            level: 5,
+            options: 1,
+          },
+          hyper: {
+            level: 9,
+            options: 5,
+          },
+          another: {
+            level: 11,
+            options: 4,
+          },
+          leggendaria: {
+            level: null,
+            options: 0,
+          },
+        },
+      },
+    },
+    {
+      genre: "FUTURE VIBES",
+      title: "HORIZON BEATZ",
+      artist: "SOUND HOLIC feat. Nana Takahashi",
+      bpm: {
+        min: 152,
+        max: null,
+      },
+      difficulty: {
+        sp: {
+          beginner: {
+            level: null,
+            options: 0,
+          },
+          normal: {
+            level: 4,
+            options: 0,
+          },
+          hyper: {
+            level: 8,
+            options: 0,
+          },
+          another: {
+            level: 10,
+            options: 0,
+          },
+          leggendaria: {
+            level: null,
+            options: 0,
+          },
+        },
+        dp: {
+          normal: {
+            level: 5,
+            options: 0,
+          },
+          hyper: {
+            level: 8,
+            options: 0,
+          },
+          another: {
+            level: 10,
+            options: 0,
+          },
+          leggendaria: {
+            level: null,
+            options: 0,
+          },
+        },
+      },
+    },
+    {
+      genre: "ブレイクコア",
+      title: "King of Tribe",
+      artist: "Numb'n'dub",
+      bpm: {
+        min: 200,
+        max: null,
+      },
+      difficulty: {
+        sp: {
+          beginner: {
+            level: null,
+            options: 0,
+          },
+          normal: {
+            level: 6,
+            options: 0,
+          },
+          hyper: {
+            level: 10,
+            options: 0,
+          },
+          another: {
+            level: 12,
+            options: 0,
+          },
+          leggendaria: {
+            level: null,
+            options: 0,
+          },
+        },
+        dp: {
+          normal: {
+            level: 6,
+            options: 0,
+          },
+          hyper: {
+            level: 10,
+            options: 0,
+          },
+          another: {
+            level: 12,
+            options: 0,
+          },
+          leggendaria: {
+            level: null,
+            options: 0,
+          },
+        },
+      },
+    },
+    {
+      genre: "STAR☆CORE",
+      title: "Meteor☆Shower",
+      artist: "Tatsunoshin",
+      bpm: {
+        min: 177,
+        max: null,
+      },
+      difficulty: {
+        sp: {
+          beginner: {
+            level: null,
+            options: 0,
+          },
+          normal: {
+            level: 5,
+            options: 1,
+          },
+          hyper: {
+            level: 9,
+            options: 1,
+          },
+          another: {
+            level: 11,
+            options: 5,
+          },
+          leggendaria: {
+            level: null,
+            options: 0,
+          },
+        },
+        dp: {
+          normal: {
+            level: 5,
+            options: 1,
+          },
+          hyper: {
+            level: 9,
+            options: 1,
+          },
+          another: {
+            level: 11,
+            options: 1,
+          },
+          leggendaria: {
+            level: null,
+            options: 0,
+          },
+        },
+      },
+    },
+    {
+      genre: "HYBRID METALCORE",
+      title: "Mighty Beat Monsterz",
+      artist: 'nora2r feat. Liqo & DD"ナカタ"Metal',
+      bpm: {
+        min: 205,
+        max: null,
+      },
+      difficulty: {
+        sp: {
+          beginner: {
+            level: null,
+            options: 0,
+          },
+          normal: {
+            level: 6,
+            options: 0,
+          },
+          hyper: {
+            level: 10,
+            options: 1,
+          },
+          another: {
+            level: 12,
+            options: 1,
+          },
+          leggendaria: {
+            level: null,
+            options: 0,
+          },
+        },
+        dp: {
+          normal: {
+            level: 6,
+            options: 0,
+          },
+          hyper: {
+            level: 10,
+            options: 0,
+          },
+          another: {
+            level: 12,
+            options: 1,
+          },
+          leggendaria: {
+            level: null,
+            options: 0,
+          },
+        },
+      },
+    },
+    {
+      genre: "GOLDEN FORCE",
+      title: "RIZING-GAMERS.",
+      artist: "BlackY",
+      bpm: {
+        min: 165,
+        max: null,
+      },
+      difficulty: {
+        sp: {
+          beginner: {
+            level: null,
+            options: 0,
+          },
+          normal: {
+            level: 5,
+            options: 5,
+          },
+          hyper: {
+            level: 10,
+            options: 5,
+          },
+          another: {
+            level: 12,
+            options: 5,
+          },
+          leggendaria: {
+            level: null,
+            options: 0,
+          },
+        },
+        dp: {
+          normal: {
+            level: 5,
+            options: 5,
+          },
+          hyper: {
+            level: 10,
+            options: 5,
+          },
+          another: {
+            level: 12,
+            options: 5,
+          },
+          leggendaria: {
+            level: null,
+            options: 0,
+          },
+        },
+      },
+    },
+    {
       genre: "DANCE POP",
       title: "Shooting Star",
       artist: "ReGLOSS",
@@ -57,6 +618,57 @@ const data: VersionData = {
       },
     },
     {
+      genre: "HARDCORE RAVE",
+      title: "SILKY BRAVE",
+      artist: "DJ Shimamura",
+      bpm: {
+        min: 178,
+        max: null,
+      },
+      difficulty: {
+        sp: {
+          beginner: {
+            level: null,
+            options: 0,
+          },
+          normal: {
+            level: 5,
+            options: 1,
+          },
+          hyper: {
+            level: 9,
+            options: 1,
+          },
+          another: {
+            level: 10,
+            options: 5,
+          },
+          leggendaria: {
+            level: null,
+            options: 0,
+          },
+        },
+        dp: {
+          normal: {
+            level: 5,
+            options: 1,
+          },
+          hyper: {
+            level: 9,
+            options: 1,
+          },
+          another: {
+            level: 10,
+            options: 5,
+          },
+          leggendaria: {
+            level: null,
+            options: 0,
+          },
+        },
+      },
+    },
+    {
       genre: "PSYTECH FULL ON",
       title: "Substance",
       artist: 'BEMANI Sound Team "めめめ"',
@@ -98,6 +710,108 @@ const data: VersionData = {
           },
           another: {
             level: 11,
+            options: 0,
+          },
+          leggendaria: {
+            level: null,
+            options: 0,
+          },
+        },
+      },
+    },
+    {
+      genre: "LIGHTNING Hi-TECH",
+      title: "THUNDERSTRIKE",
+      artist: "BlackY feat. Risa Yuzuki",
+      bpm: {
+        min: 165,
+        max: null,
+      },
+      difficulty: {
+        sp: {
+          beginner: {
+            level: null,
+            options: 0,
+          },
+          normal: {
+            level: 6,
+            options: 1,
+          },
+          hyper: {
+            level: 10,
+            options: 1,
+          },
+          another: {
+            level: 12,
+            options: 1,
+          },
+          leggendaria: {
+            level: null,
+            options: 0,
+          },
+        },
+        dp: {
+          normal: {
+            level: 6,
+            options: 1,
+          },
+          hyper: {
+            level: 10,
+            options: 1,
+          },
+          another: {
+            level: 12,
+            options: 1,
+          },
+          leggendaria: {
+            level: null,
+            options: 0,
+          },
+        },
+      },
+    },
+    {
+      genre: "JAPANESE ALTERNATIVE ROCK",
+      title: "Wizards!",
+      artist: "暁Records",
+      bpm: {
+        min: 154,
+        max: null,
+      },
+      difficulty: {
+        sp: {
+          beginner: {
+            level: null,
+            options: 0,
+          },
+          normal: {
+            level: 4,
+            options: 0,
+          },
+          hyper: {
+            level: 8,
+            options: 0,
+          },
+          another: {
+            level: 10,
+            options: 0,
+          },
+          leggendaria: {
+            level: null,
+            options: 0,
+          },
+        },
+        dp: {
+          normal: {
+            level: 4,
+            options: 0,
+          },
+          hyper: {
+            level: 8,
+            options: 0,
+          },
+          another: {
+            level: 10,
             options: 0,
           },
           leggendaria: {
@@ -200,6 +914,57 @@ const data: VersionData = {
           },
           another: {
             level: 10,
+            options: 0,
+          },
+          leggendaria: {
+            level: null,
+            options: 0,
+          },
+        },
+      },
+    },
+    {
+      genre: "DANCE POP",
+      title: "メテオラ-meteor-",
+      artist: "森羅万象",
+      bpm: {
+        min: 192,
+        max: null,
+      },
+      difficulty: {
+        sp: {
+          beginner: {
+            level: null,
+            options: 0,
+          },
+          normal: {
+            level: 5,
+            options: 0,
+          },
+          hyper: {
+            level: 9,
+            options: 0,
+          },
+          another: {
+            level: 11,
+            options: 0,
+          },
+          leggendaria: {
+            level: null,
+            options: 0,
+          },
+        },
+        dp: {
+          normal: {
+            level: 5,
+            options: 0,
+          },
+          hyper: {
+            level: 9,
+            options: 0,
+          },
+          another: {
+            level: 11,
             options: 0,
           },
           leggendaria: {
